363ce3bf68b32316cb40641b3d8682ecc3c317d1
SkyLightQP <combbm@naver.com>
2020-03-07 18:46:26 +0900

    feat: add `/notification/waitinguser` router

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,32 @@ logger.level = 'ALL';
 
 app.set('trust proxy', true);
 app.use(cors({ origin: process.env.ENDPOINT, credentials: true }));
+app.use((req, res, next) => {
+    const { method, originalUrl, ip } = req;
+    const ua = req.get('User-Agent');
+    logger.info(`${ip} ${method} ${originalUrl} - ${ua}`);
+    next();
+});
+
+const getTokenByHeader = (req: express.Request) => {
+    if (req.headers.authorization !== undefined) {
+        return req.headers.authorization.toString().split(' ')[1];
+    }
+
+    return undefined;
+};
+
+app.get('/notification/waitinguser', (req, res) => {
+    const token = getTokenByHeader(req);
+
+    if (token !== process.env.API_TOKEN) {
+        logger.warn('올바르지 않는 TOKEN으로 요청을 시도했습니다.');
+        res.sendStatus(403);
+        return;
+    }
+
+    res.sendStatus(200);
+});
 
 const port = 8080;
-app.listen(port, () => logger.info(`suft backend listening on ${port}`));
+app.listen(port, () => logger.info(`suft telegram-bot api listening on ${port}`));
